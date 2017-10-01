9b35995b0bc9964e9c3368ecad83bd557276eb6f
Chuan Qin <Chuan.Qin@capitalone.com>
2017-10-01 19:08:20 -0400

    add load balancer   - pom.xml: spring-cloud-starter-ribbon   - add @LoadBalanced  to RestTemplate Bean   - restTemplate change real url to http:// + service name

--- AmazonHub/src/main/java/com/james/mini_amazon/dao/BookDao.java
+++ AmazonHub/src/main/java/com/james/mini_amazon/dao/BookDao.java
@@ -1,31 +1,15 @@
 package com.james.mini_amazon.dao;
 
-import java.net.URI;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 
 @Service
-public class BookDao extends CommonDao{
-	
-	@Autowired
-	private DiscoveryClient client;
-	
+public class BookDao extends CommonDao {
+
 	@SuppressWarnings("unchecked")
 	public List<Object> getInventory() {
-		
-		List<ServiceInstance> list = client.getInstances("book-store");
-        if (list != null && list.size() > 0 ) {
-      	URI uri = list.get(0).getUri();
-	      	if (uri !=null ) {
-	      		return restTemplate.getForObject(uri, List.class);
-	      	}
-        }
-        return null;
-
+		return restTemplate.getForObject("http://" + "book-store", List.class);
 	}
-	
+
 }
--- AmazonHub/src/main/java/com/james/mini_amazon/dao/ElectronicDao.java
+++ AmazonHub/src/main/java/com/james/mini_amazon/dao/ElectronicDao.java
@@ -1,6 +1,7 @@
 package com.james.mini_amazon.dao;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,15 +18,15 @@ public class ElectronicDao extends CommonDao {
 	@SuppressWarnings("unchecked")
 	public List<Object> getInventory() {
 		
-		List<ServiceInstance> list = client.getInstances("electronic-store");
-        if (list != null && list.size() > 0 ) {
-      	URI uri = list.get(0).getUri();
-	      	if (uri !=null ) {
-	      		return restTemplate.getForObject(uri, List.class);
-	      	}
-        }
-        return null;
-
+//		List<ServiceInstance> list = client.getInstances("electronic-store");
+//        if (list != null && list.size() > 0 ) {
+//      	URI uri = list.get(0).getUri();
+//	      	if (uri !=null ) {
+//	      		return restTemplate.getForObject(uri, List.class);
+//	      	}
+//        }
+//        return null;
+		return new ArrayList<>();
 	}
 
 }
