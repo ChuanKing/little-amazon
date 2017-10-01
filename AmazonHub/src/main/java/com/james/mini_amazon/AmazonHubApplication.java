8ce16d8b094e105eb06b979401522a752faac198
Chuan Qin <Chuan.Qin@capitalone.com>
2017-10-01 18:45:43 -0400

    add eureka and config server

--- AmazonHub/src/main/java/com/james/mini_amazon/AmazonHubApplication.java
+++ AmazonHub/src/main/java/com/james/mini_amazon/AmazonHubApplication.java
@@ -2,8 +2,10 @@ package com.james.mini_amazon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AmazonHubApplication {
 
 	public static void main(String[] args) {
--- AmazonHub/src/main/java/com/james/mini_amazon/controller/AmazonHubController.java
+++ AmazonHub/src/main/java/com/james/mini_amazon/controller/AmazonHubController.java
@@ -2,8 +2,11 @@ package com.james.mini_amazon.controller;
 
 import java.util.List;
 
+import javax.ws.rs.core.MediaType;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.james.mini_amazon.service.AmazonHubService;
@@ -14,7 +17,7 @@ public class AmazonHubController {
 	@Autowired
 	private AmazonHubService amazonHubService;
 	
-	@RequestMapping("/inventory")
+	@RequestMapping(value = "/inventory", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON)
 	public List<Object> getInventory() {
 		return amazonHubService.getInventory();
 	}
