c595883de2b8fac9937bd7072bf1a72827f3fa2a
Chuan Qin <Chuan.Qin@capitalone.com>
2017-10-01 18:55:25 -0400

    use eureka to find micro service

--- AmazonHub/src/main/java/com/james/mini_amazon/dao/ElectronicDao.java
+++ AmazonHub/src/main/java/com/james/mini_amazon/dao/ElectronicDao.java
@@ -1,15 +1,31 @@
 package com.james.mini_amazon.dao;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ElectronicDao extends CommonDao {
 
+	@Autowired
+	private DiscoveryClient client;
+	
 	@SuppressWarnings("unchecked")
 	public List<Object> getInventory() {
-		return restTemplate.getForObject("http://localhost:58521/", List.class);
+		
+		List<ServiceInstance> list = client.getInstances("electronic-store");
+        if (list != null && list.size() > 0 ) {
+      	URI uri = list.get(0).getUri();
+	      	if (uri !=null ) {
+	      		return restTemplate.getForObject(uri, List.class);
+	      	}
+        }
+        return null;
+
 	}
 
 }
