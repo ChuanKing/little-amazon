70758bb0d91f25cecece793f6f8a8a3973fa3a87
Chuan Qin <Chuan.Qin@capitalone.com>
2017-10-01 14:14:15 -0400

    regist home and kitchen server to eureka

--- HomeKitchenStore/src/main/java/com/james/mini_amazon/HomeKitchenStoreApplication.java
+++ HomeKitchenStore/src/main/java/com/james/mini_amazon/HomeKitchenStoreApplication.java
@@ -2,8 +2,10 @@ package com.james.mini_amazon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class HomeKitchenStoreApplication {
 
 	public static void main(String[] args) {
