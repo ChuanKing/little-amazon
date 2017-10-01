9ccbcf0cfc1e48bd3c3a61c4fb17ebbc0990709d
Chuan Qin <Chuan.Qin@capitalone.com>
2017-10-01 19:11:38 -0400

    add load balancer version 2   - pom.xml: spring-cloud-starter-ribbon   - add LoadBalancerClient loadBalancer to dao   - ServiceInstance instance = loadBalancer.choose(SERVICE_NAME);   - restTemplate.getForObject(instance.getUri(), List.class);

--- AmazonHub/src/main/java/com/james/mini_amazon/dao/BookDao.java
+++ AmazonHub/src/main/java/com/james/mini_amazon/dao/BookDao.java
@@ -2,14 +2,23 @@ package com.james.mini_amazon.dao;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookDao extends CommonDao {
+	
+	private final static String SERVICE_NAME = "book-store";
+	
+	@Autowired 
+	private LoadBalancerClient loadBalancer;
 
 	@SuppressWarnings("unchecked")
 	public List<Object> getInventory() {
-		return restTemplate.getForObject("http://" + "book-store", List.class);
+		ServiceInstance instance = loadBalancer.choose(SERVICE_NAME);
+   		return restTemplate.getForObject(instance.getUri(), List.class);
 	}
 
 }
